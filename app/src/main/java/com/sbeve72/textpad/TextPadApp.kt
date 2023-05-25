19270a00edb198d937ed906f377ff2fe53670148
dev-skumar <131763628+dev-skumar@users.noreply.github.com>
2023-05-25 17:35:51 +0530

    Implement initial setup for 'Dependency Injection' with hilt

---
+++ app/src/main/java/com/sbeve72/textpad/TextPadApp.kt
@@ -0,0 +1,7 @@
+package com.sbeve72.textpad
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TextPadApp: Application()
---
+++ app/src/main/java/com/sbeve72/textpad/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.sbeve72.textpad.di
+
+import android.app.Application
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    /**
+     *  Dependecy provider functions will go here
+     */
+
+//    e.g.
+
+//    @Provides
+//    @Singleton
+//    fun provideExampleDependency(app: Application): ExampleDependency {
+//        return exampleDependecy
+//    }
+
+
+}
